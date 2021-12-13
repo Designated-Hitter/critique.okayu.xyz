1568d33e21eaca4d524a1e3f6b39c983fc409f27
Designated-Hitter <dosadola@gmail.com>
2021-12-13 13:47:42 +0900

    URLsearchParams  오류 수정, result.data.success if문 수정

--- emailVerify.js
+++ emailVerify.js
@@ -1,17 +1,18 @@
 ;(async () => {
-    const vCode = new URLSearchParams.get(code);
+    const vCode = new URLSearchParams(location.search).get("code");
     const result = await axios({
         method: "POST",
         url: `https://api.critique.okayu.xyz/user/verify?code=${vCode}`
     })
 
     if(result.data.success){
-        alert(result.data.error);
-        return;
-    } else {
         alert(result.data.message);
         location.href = "index.html";
+    } else {
+        alert(result.data.error);
+        return;
+        
     };
 
     
-});
+})();
