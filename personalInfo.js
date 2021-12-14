4973a20d5cbca565b4018e02a066255e2dea7d2c
Designated-Hitter <dosadola@gmail.com>
2021-12-14 15:10:31 +0900

    회원탈퇴기능 수정

--- personalInfo.js
+++ personalInfo.js
@@ -36,26 +36,43 @@ async function tryUpdateInfo() {
 
 }
 async function tryDeleteInfo() {
-    const token = localStorage.getItem("token");
-    const response = confirm('정말로 탈퇴하시겠습니까?')
+    
+    const response = confirm('정말로 탈퇴하시겠습니까? 지금까지 작성하신 모든 서평과 개인정보는 삭제됩니다.')
 
     if (response) {
-        const passwordInput = prompt('비밀번호를 입력해주십시오.');
+        const withdraw = document.querySelector('div.withdraw');
+        const lastPassword = document.createElement('input');
+        lastPassword.type = "password";
+        lastPassword.name = 'last-password';
+        lastPassword.placeholder = "비밀번호를 입력해주십시오."
+        const leaveButton = document.createElement('button');
+        leaveButton.classList.add('leave');
+        leaveButton.innerText = "탈퇴"
+        leaveButton.addEventListener("click", leave)
+        withdraw.append(lastPassword, leaveButton);
+        
 
-        const result = await axios({
-            method: "DELETE",
-            url: "https://api.critique.okayu.xyz/user/delete",
-            data: { password: passwordInput },
-            headers: { Authorization: token }
-        });
-
-        if (!result.data.success) {
-            alert(result.data.error);
-            return;
-        } else {
-            alert(result.data.message);
-            localStorage.removeItem("token");
-            location.href = 'index.html';
-        };
     };
 };
+
+async function leave() {
+    const lastPassword = document.querySelector('input[name="last-password"]');
+    const token = localStorage.getItem("token");
+    const passwordInput = lastPassword.value;
+
+    const result = await axios({
+        method: "DELETE",
+        url: "https://api.critique.okayu.xyz/user/delete",
+        data: { password: passwordInput },
+        headers: { Authorization: token }
+    });
+
+    if (!result.data.success) {
+        alert(result.data.error);
+        return;
+    } else {
+        alert(result.data.message);
+        localStorage.removeItem("token");
+        location.href = 'index.html';
+    };
+}
