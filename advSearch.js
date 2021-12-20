614c1375f58ca2468d1055095a168ff71a3a97ff
Designated-Hitter <dosadola@gmail.com>
2021-12-20 23:37:14 +0900

    고급 검색기능을 DB에 데이터를 추가하고 일반 검색으로 넘기는 형식으로 변경

--- advSearch.js
+++ advSearch.js
@@ -14,97 +14,99 @@
         labelSearching.innerText = "검색결과가 없습니다.";
         return;
     }
-    for(const item of list){
-        const itemId = item.item_id;
-        const cover = item.cover;
 
-        const a = document.createElement('a');
-        a.classList.add(`to-entire-critique-${itemId}`);
-        a.href = `readManyCritiques.html?book_id=${itemId}`;
+    location.href = `search.html?keyword=${searchWord}`
+    // for(const item of list){
+    //     const itemId = item.item_id;
+    //     const cover = item.cover;
 
-        const resultCover = document.createElement('img');
-        resultCover.classList.add('cover');
-        resultCover.src = cover;
+    //     const a = document.createElement('a');
+    //     a.classList.add(`to-entire-critique-${itemId}`);
+    //     a.href = `readManyCritiques.html?book_id=${itemId}`;
+
+    //     const resultCover = document.createElement('img');
+    //     resultCover.classList.add('cover');
+    //     resultCover.src = cover;
         
-        const title = item.title;
-        const resultTitle = document.createElement('div');
-        resultTitle.classList.add('title');
-        resultTitle.innerText = "제목: " + title;
-        a.append(resultCover, resultTitle);
+    //     const title = item.title;
+    //     const resultTitle = document.createElement('div');
+    //     resultTitle.classList.add('title');
+    //     resultTitle.innerText = "제목: " + title;
+    //     a.append(resultCover, resultTitle);
 
-        const author = item.author;
-        const resultAuthor = document.createElement('div');
-        resultAuthor.classList.add('author');
-        resultAuthor.innerText = "저자: " + author;
+    //     const author = item.author;
+    //     const resultAuthor = document.createElement('div');
+    //     resultAuthor.classList.add('author');
+    //     resultAuthor.innerText = "저자: " + author;
 
-        const category = item.category_name;
-        const resultCategory = document.createElement('div');
-        resultCategory.classList.add('category');
-        resultCategory.innerText = "분류: " + category;
+    //     const category = item.category_name;
+    //     const resultCategory = document.createElement('div');
+    //     resultCategory.classList.add('category');
+    //     resultCategory.innerText = "분류: " + category;
 
-        const publisher = item.publisher;
-        const resultPublisher = document.createElement('div');
-        resultPublisher.classList.add('publisher');
-        resultPublisher.innerText = "출판사: " + publisher ;
+    //     const publisher = item.publisher;
+    //     const resultPublisher = document.createElement('div');
+    //     resultPublisher.classList.add('publisher');
+    //     resultPublisher.innerText = "출판사: " + publisher ;
 
-        const writeComment = document.createElement('button');
-        writeComment.addEventListener("click", () => {writeCritique(itemId, author, cover)});
-        writeComment.innerText = "서평 쓰기";
+    //     const writeComment = document.createElement('button');
+    //     writeComment.addEventListener("click", () => {writeCritique(itemId, author, cover)});
+    //     writeComment.innerText = "서평 쓰기";
 
-        const searchedBook = document.createElement('div');
-        searchedBook.classList.add("searched");
+    //     const searchedBook = document.createElement('div');
+    //     searchedBook.classList.add("searched");
 
-        searchedBook.append(a, resultAuthor, resultCategory, resultPublisher, writeComment);
-        searchResult.append(searchedBook);
+    //     searchedBook.append(a, resultAuthor, resultCategory, resultPublisher, writeComment);
+    //     searchResult.append(searchedBook);
 
-        labelSearching.innerText = "";
-    }
+    //     labelSearching.innerText = "";
+    // }
     
 
     
 
     //페이지네이션
-    const numberOfSearches = result.data.number_of_searches;
-    const firstPageOfAll = 1;
-    const lastPageOfAll = Math.ceil(numberOfSearches / 10);
-    console.log(lastPageOfAll)
-    const firstPageOfThis = (Math.ceil(page / 10) - 1) * 10 + 1;
-    console.log(firstPageOfThis)
-    const lastPageOfThis = Math.ceil(page / 10) * 10;
-    console.log(lastPageOfThis)
-    const displayFirstPage = firstPageOfAll > firstPageOfThis ? firstPageOfAll : firstPageOfThis;
-    console.log(displayFirstPage)
-    const displayLastPage = lastPageOfAll > lastPageOfThis ? lastPageOfThis : lastPageOfAll;
-    console.log(displayLastPage)
+    // const numberOfSearches = result.data.number_of_searches;
+    // const firstPageOfAll = 1;
+    // const lastPageOfAll = Math.ceil(numberOfSearches / 10);
+    // console.log(lastPageOfAll)
+    // const firstPageOfThis = (Math.ceil(page / 10) - 1) * 10 + 1;
+    // console.log(firstPageOfThis)
+    // const lastPageOfThis = Math.ceil(page / 10) * 10;
+    // console.log(lastPageOfThis)
+    // const displayFirstPage = firstPageOfAll > firstPageOfThis ? firstPageOfAll : firstPageOfThis;
+    // console.log(displayFirstPage)
+    // const displayLastPage = lastPageOfAll > lastPageOfThis ? lastPageOfThis : lastPageOfAll;
+    // console.log(displayLastPage)
 
-    const pagination = document.querySelector("footer.page");
+    // const pagination = document.querySelector("footer.page");
 
-    if(displayFirstPage !== firstPageOfAll) {
-        const aPage = document.createElement('a');
-        aPage.href = `advSearch.html?keyword=${searchWord}&page=${displayFirstPage - 1}`;
-        aPage.innerText = "이전 페이지";
+    // if(displayFirstPage !== firstPageOfAll) {
+    //     const aPage = document.createElement('a');
+    //     aPage.href = `advSearch.html?keyword=${searchWord}&page=${displayFirstPage - 1}`;
+    //     aPage.innerText = "이전 페이지";
 
-        pagination.append(aPage);
-    }
+    //     pagination.append(aPage);
+    // }
 
-    for (let p = displayFirstPage; p <= displayLastPage; p++) {
-        const aPage = document.createElement('a');
-        aPage.href = `advSearch.html?keyword=${searchWord}&page=${p}`;
-        aPage.innerText = p;
+    // for (let p = displayFirstPage; p <= displayLastPage; p++) {
+    //     const aPage = document.createElement('a');
+    //     aPage.href = `advSearch.html?keyword=${searchWord}&page=${p}`;
+    //     aPage.innerText = p;
         
-        if(p === page) {
-            aPage.classList.add('now-page');
-        }
-        pagination.append(aPage);
-    }
+    //     if(p === page) {
+    //         aPage.classList.add('now-page');
+    //     }
+    //     pagination.append(aPage);
+    // }
 
-    if (displayLastPage !== lastPageOfAll) {
-        const aPage = document.createElement('a');
-        aPage.href = `advSearch.html?keyword=${searchWord}&page=${displayLastPage + 1}`;
-        aPage.innerText = "다음 페이지";
+    // if (displayLastPage !== lastPageOfAll) {
+    //     const aPage = document.createElement('a');
+    //     aPage.href = `advSearch.html?keyword=${searchWord}&page=${displayLastPage + 1}`;
+    //     aPage.innerText = "다음 페이지";
 
-        pagination.append(aPage);
-    }
+    //     pagination.append(aPage);
+    // }
 
 })();
 
